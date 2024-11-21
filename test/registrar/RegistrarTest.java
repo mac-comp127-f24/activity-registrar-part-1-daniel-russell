0214b1ab146c13e61339f3f5e2c7d13549526978
daniel-quintero00 <dquinter@macalester.edu>
2024-11-21 17:02:31 -0600

    Remove test added

--- test/registrar/RegistrarTest.java
+++ test/registrar/RegistrarTest.java
@@ -75,13 +75,6 @@ class RegistrarTest {
         assertFalse(comp127.getRoster().contains(sally));
     }
 
-    @Test
-    void clientsCannotModifyCourses() {
-       List<Course> courses = sally.getCourses();
-       courses.add(comp127);
-    }
-
-
     // ------ Post-test invariant check ------
     //
     // This is a bit persnickety for day-to-day testing, but these kinds of checks are appropriate
